0d6e4faa24a59ecf72c4d3319c63ad3d65a52959
Alexandr <56019760+Sistem-Pack@users.noreply.github.com>
2023-12-23 22:32:18 +0600

    19 sprint next

--- app/src/main/java/com/practicum/playlistmaker/ui/root/RootActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/ui/root/RootActivity.kt
@@ -3,6 +3,7 @@ package com.practicum.playlistmaker.ui.root
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.practicum.playlistmaker.R
 import com.practicum.playlistmaker.databinding.ActivityRootBinding
@@ -19,6 +20,16 @@ class RootActivity : AppCompatActivity() {
         findFragmentById(R.id.rootFragmentContainerView) as NavHostFragment
         val navController = navHostFragment.navController
 
-        binding.bottomNavigationView.setupWithNavController(navController)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if ((destination.id == R.id.searchFragment) ||
+                (destination.id == R.id.settingsFragment)) {
+                binding.toolbar.navigationIcon = null
+            }
+        }
+
+       binding.bottomNavigationView.setupWithNavController(navController)
+
     }
 }
